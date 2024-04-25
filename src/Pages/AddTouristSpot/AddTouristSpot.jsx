d940397705aeec6293dd11592a7aa28919976532
sujonifty <sujonifty@gmail.com>
2024-04-26 01:13:00 +0600

    add tourist form completed

--- src/Pages/AddTouristSpot/AddTouristSpot.jsx
+++ src/Pages/AddTouristSpot/AddTouristSpot.jsx
@@ -1,8 +1,100 @@
 
 const AddTouristSpot = () => {
+    const handleAddSpot = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const country = e.target.country.value;
+        const location = e.target.location.value;
+        const photo = e.target.photo.value;
+        const cost = e.target.cost.value;
+        const seasonality = e.target.seasonality.value;
+        const time = e.target.time.value;
+        const visitor = e.target.visitor.value;
+        const description = e.target.description.value;
+        const TouristInfo ={name, country, location, photo, cost, seasonality, time, visitor, description}
+        console.log(TouristInfo)
+    }
     return (
         <div>
-            <h1>Add Tourist Spot</h1>
+
+            <div className="text-center">
+                <h1 className="text-5xl w-full lg:w-3/4 mx-auto font-bold">Add New Tourist Spot</h1>
+                <p className="py-6">It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
+            </div>
+            <div className="hero min-h-screen bg-[#F4F3F0]">
+
+                <form onSubmit={handleAddSpot} className="card-body">
+                    <div className="flex gap-5">
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Tourists Spot Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Enter Tourists Spot Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Country Name</span>
+                            </label>
+                            <input type="text" name="country" placeholder="Enter Country Name" className="input input-bordered" required />
+                        </div>
+                    </div>
+                    <div className="flex gap-5">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Location</span>
+                            </label>
+                            <input type="text" name="location" placeholder="Enter Location" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">PhotoURL</span>
+                            </label>
+                            <input type="text" name="photo" placeholder="Enter PhotoURL" className="input input-bordered" required />
+                        </div>
+
+                    </div>
+                    <div className="flex gap-5">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Average Cost</span>
+                            </label>
+                            <input type="text" name="cost" placeholder="Enter Average Cost" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Seasonality</span>
+                            </label>
+                            <input type="text" name="seasonality " placeholder="like summer, winter" className="input input-bordered" required />
+                        </div>
+
+                    </div>
+                    <div className="flex gap-5">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Travel Time</span>
+                            </label>
+                            <input type="text" name="time " placeholder="like summer, winter" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Total Visitors Per Year</span>
+                            </label>
+                            <input type="text" name="visitor " placeholder="Enter Total Visitor" className="input input-bordered" required />
+                        </div>
+
+                    </div>
+
+                    <label className="label">
+                        <span className="label-text">Short Description</span>
+                    </label>
+                    <textarea name="description" placeholder="Write Short Description" className="textarea textarea-bordered textarea-xs w-full" required></textarea>
+
+                    <div className="form-control mt-6">
+                        <input type="submit" className="btn bg-[#D2B48C]" value="Add Tourist Spot" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,37 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
+    const navLinks = <>
+        <NavLink className="btn"  to="/">Home</NavLink>
+        <NavLink className="btn" to="/addSpot">Add Tourists Spot</NavLink>
+        <NavLink className="btn" to="/allSpot">All Tourists Spot</NavLink>
+        <NavLink className="btn" to="/myList">My Lists</NavLink>
+    </>
     return (
         <div>
-            <h1>Navbar</h1>
+            <div className="navbar bg-base-100 shadow-xl">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            
+                            {navLinks}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">Globify</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 space-x-4">
+                        {navLinks}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
+            </div>
         </div>
     );
 };
