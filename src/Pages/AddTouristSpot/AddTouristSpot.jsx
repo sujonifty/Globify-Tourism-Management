d9b336bb12a3df7a11259a0b0735e519e876fae5
sujonifty <sujonifty@gmail.com>
2024-04-26 08:10:32 +0600

    completed add tourist spot client section

--- src/Pages/AddTouristSpot/AddTouristSpot.jsx
+++ src/Pages/AddTouristSpot/AddTouristSpot.jsx
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 
 const AddTouristSpot = () => {
     const handleAddSpot = (e) => {
@@ -7,12 +8,32 @@ const AddTouristSpot = () => {
         const location = e.target.location.value;
         const photo = e.target.photo.value;
         const cost = e.target.cost.value;
-        const seasonality = e.target.seasonality.value;
-        const time = e.target.time.value;
-        const visitor = e.target.visitor.value;
+        const season = e.target.season.value;
+        const travelTime = e.target.travelTime.value;
+        const totalVisitors = e.target.totalVisitors.value;
         const description = e.target.description.value;
-        const TouristInfo ={name, country, location, photo, cost, seasonality, time, visitor, description}
-        console.log(TouristInfo)
+        const touristInfo ={name, country, location,photo,cost, season,travelTime,totalVisitors, description}
+        // console.log(name, country, location,photo,cost, season,totalVisitors,travelTime,description)
+        console.log(touristInfo);
+        fetch('http://localhost:5000/touristSpot',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(touristInfo)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Tourist spot added successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Cool'
+                  })
+            }
+        })
     }
     return (
         <div>
@@ -65,7 +86,7 @@ const AddTouristSpot = () => {
                             <label className="label">
                                 <span className="label-text">Seasonality</span>
                             </label>
-                            <input type="text" name="seasonality " placeholder="like summer, winter" className="input input-bordered" required />
+                            <input type="text" name="season" placeholder="Like summer, winter" className="input input-bordered" required />
                         </div>
 
                     </div>
@@ -74,21 +95,23 @@ const AddTouristSpot = () => {
                             <label className="label">
                                 <span className="label-text">Travel Time</span>
                             </label>
-                            <input type="text" name="time " placeholder="like summer, winter" className="input input-bordered" required />
+                            <input type="text" name="travelTime" placeholder="Like 5 days" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Total Visitors Per Year</span>
+                                <span className="label-text">Total Visitors</span>
                             </label>
-                            <input type="text" name="visitor " placeholder="Enter Total Visitor" className="input input-bordered" required />
+                            <input type="text" name="totalVisitors" placeholder="Enter visitors number" className="input input-bordered" required />
                         </div>
 
                     </div>
-
-                    <label className="label">
-                        <span className="label-text">Short Description</span>
-                    </label>
-                    <textarea name="description" placeholder="Write Short Description" className="textarea textarea-bordered textarea-xs w-full" required></textarea>
+                    
+                    <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Short Description</span>
+                            </label>
+                            <input type="text" name="description" placeholder="write description" className="input input-bordered" required />
+                        </div>
 
                     <div className="form-control mt-6">
                         <input type="submit" className="btn bg-[#D2B48C]" value="Add Tourist Spot" />
